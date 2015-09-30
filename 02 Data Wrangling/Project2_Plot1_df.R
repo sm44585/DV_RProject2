927e5c4c2013c5e8b4a616f600a75f420da71a1e
Spencer Muncey <scmuncey@yahoo.com>
2015-09-30 13:51:10 -0500

    Update based on meeting before class

--- 02 Data Wrangling/Project2_Plot1_df.R
+++ 02 Data Wrangling/Project2_Plot1_df.R
@@ -3,15 +3,17 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 
-Plot1_df <- df %>% group_by (EDUCATION, HUSBY) %>% summarise(mean_kids = mean(KIDSLT6 + KIDS618)) %>% arrange(desc(EDUCATION))
 
+Plot1_df <- df %>% group_by (EDUCATION, HUSBY) %>% summarise(mean_kids = mean(KIDSLT6 + KIDS618))
+
+# plot of how education and husbands income influences number of kids
 ggplot() +
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
   facet_grid(.~EDUCATION, labeller=label_both) + 
   labs(title='How Education level and Income influences number of children') +
-  labs(x="Number of Kids", y=paste("Husbands Income")) +
+  labs(x="Number of Kids", y=paste("Husbands Income(thousands of dollars)")) +
   layer(data=Plot1_df, 
         mapping=aes(x=as.numeric(mean_kids), y=as.numeric(as.character(HUSBY))), 
         stat="identity", 
