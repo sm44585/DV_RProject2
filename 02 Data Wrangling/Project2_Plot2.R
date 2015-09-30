43ae1fba7258b169dc77f8b2a7c5da66709fac1b
Spencer Muncey <scmuncey@yahoo.com>
2015-09-30 17:17:11 -0500

    Deleted unnecessary data wrangling plot files

--- 02 Data Wrangling/Project2_Plot2.R
+++
@@ -1,5 +0,0 @@
-require(tidyr)
-require(dplyr)
-require(ggplot2)
-
-df %>% mutate(husby_percent = cume_dist(KIDS618)) %>% filter(husby_percent <= .20 | husby_percent >= .80) %>% ggplot(aes(x = KIDS618, y = WHRSWK)) + facet_grid(.~REGION, labeller=label_both)+labs(title="")+labs(x="Number of kids 6–18 years old")+ geom_point()
